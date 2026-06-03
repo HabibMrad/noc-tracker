89972818c7e3b2fc7203c9afe4b27a4724e07907
Habib <30547523+HabibMrad@users.noreply.github.com>
2026-06-04 01:21:51 +0300

    feat: add photos API client

---
+++ frontend/src/api/photos.js
@@ -0,0 +1,17 @@
+import client from "./client"
+
+export const uploadPhotos = (checkinId, files) => {
+  const form = new FormData()
+  files.forEach((f) => form.append("files", f))
+  return client.post(`/checkins/${checkinId}/photos`, form, {
+    headers: { "Content-Type": "multipart/form-data" },
+  }).then((r) => r.data)
+}
+
+export const getPhotos = (checkinId) =>
+  client.get(`/checkins/${checkinId}/photos`).then((r) => r.data)
+
+export const deletePhoto = (photoId) =>
+  client.delete(`/photos/${photoId}`)
+
+export const getPhotoUrl = (filename) => `/api/photos/${filename}`
